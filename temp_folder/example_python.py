8ebed184f97535daa7ec485b0b47e923ff6d375d
Himanshu Kandpal <kandpallucky@gmail.com>
2024-05-16 16:02:45 +0530

    added doc strings to the functions

--- temp_folder/example_python.py
+++ temp_folder/example_python.py
@@ -1,4 +1,7 @@
 def add_numbers(a, b):
+    """
+    Returns addition of 2 numbers 
+    """
     return a + b
 
 # 1. extend add_numbers to allow any arbitrary number of parameters.
@@ -7,4 +10,7 @@ def add_numbers(a, b):
 # 4. Add documentation
 
 def average_of_numbers(a, b):
+    """
+    Returns average of two numbers
+    """
     return (a+b)/2
