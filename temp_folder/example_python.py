5db8f84604b0640305dd6ff9360aea38a09af052
Himanshu Kandpal <kandpallucky@gmail.com>
2024-05-16 15:44:15 +0530

    Things to change

--- temp_folder/example_python.py
+++ temp_folder/example_python.py
@@ -1,5 +1,10 @@
 def add_numbers(a, b):
     return a + b
 
+# 1. extend add_numbers to allow any arbitrary number of parameters.
+# 2. Investigate security exploit of add numbers
+# 3. Make it more performant
+# 4. Add documentation
+
 def average_of_numbers(a, b):
     return (a+b)/2
